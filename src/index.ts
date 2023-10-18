5fd2d98aec20fe4d99dcd127c8fd29124d37af3f
ritchtablet <ritchtablet@gmail.com>
2023-10-17 19:26:43 -0600

    feat: add conventional commits and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import { myDataSource } from "./app-data-source"
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { myDataSource } from "./app-data-source";
 
 dotenv.config();
 
 myDataSource
-    .initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization:", err)
-    })
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+  });
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
